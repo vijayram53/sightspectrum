748b6797b580003916d559215703410733c3d5e1
unknown <vijayaraman.sightspectrum@gmail.com>
2023-05-22 18:42:04 +0530

    first commit

---
+++ task_6 22.05.23/functions SL.py
@@ -0,0 +1,20 @@
+Example Code:
+# Function definition
+def greet(name):
+    message = f"Hello, {name}!"
+    return message
+
+# Function call
+result = greet("Alice")
+print(result)  # Output: Hello, Alice!
+
+Example code:
+
+def add_numbers(a, b):
+    return a + b
+
+result1 = add_numbers(5, 3)
+print(result1)  # Output: 8
+
+result2 = add_numbers(10, 7)
+print(result2)  # Output: 17
