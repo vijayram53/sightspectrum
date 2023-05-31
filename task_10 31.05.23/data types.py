ab5fb31ab0fc85194eaa809a5fc7b1077d86cc69
unknown <vijayaraman.sightspectrum@gmail.com>
2023-05-31 17:18:17 +0530

    first commit

---
+++ task_10 31.05.23/data types.py
@@ -0,0 +1,111 @@
+#Lists
+List = []
+print("Blank List: ")
+print(List)
+  
+# Creating a List of numbers
+List = [10, 20, 14]
+print("\nList of numbers: ")
+print(List)
+  
+# Creating a List of strings and accessing
+# using index
+List = ["hello", "For", "hello"]
+print("\nList Items: ")
+print(List[0])
+print(List[2])
+
+#tuples
+# Creating an empty Tuple
+Tuple1 = ()
+print("Initial empty Tuple: ")
+print(Tuple1)
+ 
+# Creating a Tuple
+# with the use of string
+Tuple1 = ('hello', 'For')
+print("\nTuple with the use of String: ")
+print(Tuple1)
+ 
+# Creating a Tuple with
+# the use of list
+list1 = [1, 2, 4, 5, 6]
+print("\nTuple using List: ")
+print(tuple(list1))
+ 
+# Creating a Tuple
+# with the use of built-in function
+Tuple1 = tuple('hello')
+print("\nTuple with the use of function: ")
+print(Tuple1)
+
+#sets
+
+
+# Creating a Set
+set1 = set()
+print("Initial blank Set: ")
+print(set1)
+ 
+# Creating a Set with
+# the use of a String
+set1 = set("hello world")
+print("\nSet with the use of String: ")
+print(set1)
+ 
+# Creating a Set with
+# the use of Constructor
+# (Using object to Store String)
+String = 'hello world'
+set1 = set(String)
+print("\nSet with the use of an Object: " )
+print(set1)
+ 
+# Creating a Set with
+# the use of a List
+set1 = set(["hello", "For", "hello"])
+print("\nSet with the use of List: ")
+print(set1)
+
+#dictionary
+
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'hello', 2: 'For', 3: 'hello'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+  
+# Creating a Dictionary
+Dict = {'Name': 'hello', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
+
+#strings
+
+
+String1 = 'Welcome to the helloWorld'
+print("String with the use of Single Quotes: ")
+print(String1)
+ 
+# Creating a String
+# with double Quotes
+String1 = "I'm a world"
+print("\nString with the use of Double Quotes: ")
+print(String1)
+ 
+# Creating a String
+# with triple Quotes
+String1 = '''I'm a helloand I live in a world of "world"'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
+ 
+# Creating String with triple
+# Quotes allows multiple lines
+String1 = '''hello
+            For
+            Life'''
+print("\nCreating a multiline String: ")
+print(String1)
+
+
+
