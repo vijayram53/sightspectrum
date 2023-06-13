b9246aeb4687f0d321ba535578c6ffb4f63d1f97
unknown <vijayaraman.sightspectrum@gmail.com>
2023-06-13 18:02:55 +0530

    first commit

---
+++ task_16 13.06.23/class&objects.py
@@ -0,0 +1,64 @@
+#Example code
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+#Example code:
+
+
+
+class Person:  
+    def __init__(self, name, age):  
+         
+        self.name = name  
+        self.age = age  
+    def greet(self):  
+        
+        print("Hello, my name is " + self.name)
+
+
+#Example code:
+class Room:
+    length = 0.0
+    breadth = 0.0
+    
+
+    def calculate_area(self):
+        print("Area of Room =", self.length * self.breadth)
+
+
+study_room = Room()
+
+ 
+study_room.length = 42.5
+study_room.breadth = 30.8
+
+
+study_room.calculate_area()
+
+
+#Example code:
+
+class Person:
+  def __init__(varun, name, age):
+    varun.name = name
+    varun.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+
+
+        
